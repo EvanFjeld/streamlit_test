845fc349ac76956a98d4e0661d28cebf91fcf58a
EvanFjeld <evanmfjeld@gmail.com>
2023-07-25 23:27:29 -0700

    Update homepage.py

--- homepage.py
+++ homepage.py
@@ -170,33 +170,33 @@ def data_frame_demo():
 
     @st.cache_data
     def get_UN_data():
-        AWS_BUCKET_URL = "http://streamlit-demo-data.s3-us-west-2.amazonaws.com"
-        df = pd.read_csv(AWS_BUCKET_URL + "/agri.csv.gz")
+        #AWS_BUCKET_URL = "http://streamlit-demo-data.s3-us-west-2.amazonaws.com"
+        AWS_BUCKET_URL = "https://carbon-forecaster-capstone-s3.s3.us-west-2.amazonaws.com"
+        df = pd.read_csv(AWS_BUCKET_URL + "/streamlit_data/test.csv")
         return df.set_index("Region")
 
     try:
         df = get_UN_data()
         countries = st.multiselect(
-            "Choose countries", list(df.index), ["China", "United States of America"]
+            "Choose location", list(df.index), ["LoggingScar"]
         )
         if not countries:
             st.error("Please select at least one country.")
         else:
-            data = df.loc[countries]
-            data /= 1000000.0
-            st.write("### Gross Agricultural Production ($B)", data.sort_index())
+            data = df.loc[LoggingScar]
+            st.write("### Gpp", data.sort_index())
 
             data = data.T.reset_index()
             data = pd.melt(data, id_vars=["index"]).rename(
-                columns={"index": "year", "value": "Gross Agricultural Product ($B)"}
+                columns={"index": "date", "Gpp": "Gpp"}
             )
             chart = (
                 alt.Chart(data)
                 .mark_area(opacity=0.3)
                 .encode(
-                    x="year:T",
-                    y=alt.Y("Gross Agricultural Product ($B):Q", stack=None),
-                    color="Region:N",
+                    x="date:T",
+                    y=alt.Y("Gpp:Q", stack=None),
+                    color="Actual_Forecast:N",
                 )
             )
             st.altair_chart(chart, use_container_width=True)
