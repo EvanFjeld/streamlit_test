7d7fa10148a01e1ddae38082b1d1d3f1386a46f7
EvanFjeld <evanmfjeld@gmail.com>
2023-07-25 19:38:27 -0700

    Update ux_test.py

--- ux_test.py
+++ ux_test.py
@@ -2,9 +2,11 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('Boreal Forecast Carbon Forecaster')
+
 
 # Add a number input form
 lat = st.number_input("Latitude", min_value=0, max_value=150, value=30, step=1)
 long = st.number_input("Latitude", min_value=0, max_value=150, value=30, step=1)
+
+st.title('Boreal Forecast Carbon Forecaster')
 st.write("Latitude / Longitude: ", lat, " / ", long)
