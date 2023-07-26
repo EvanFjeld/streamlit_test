88f3d905e284bb342da705906275f1929f6ca3da
EvanFjeld <evanmfjeld@gmail.com>
2023-07-25 19:39:47 -0700

    Update ux_test.py

--- ux_test.py
+++ ux_test.py
@@ -9,4 +9,5 @@ lat = st.number_input("Latitude", min_value=0, max_value=150, value=30, step=1)
 long = st.number_input("Latitude", min_value=0, max_value=150, value=30, step=1)
 
 
-st.write("Latitude / Longitude: ", lat)
+st.write("Latitude: ", lat)
+st.write("Longitude: ", long)
