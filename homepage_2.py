18a9b1b43eefe17807b244222423028131a820ec
EvanFjeld <evanmfjeld@gmail.com>
2023-07-28 15:45:42 -0700

    Update homepage_2.py

--- homepage_2.py
+++ homepage_2.py
@@ -15,6 +15,9 @@ def intro():
     )
 
 def single_location_page():
+    if "selected_location" not in st.session_state:
+        st.session_state.selected_location = None
+
     location_name = st.selectbox("Choose a location", single_location_to_funcs.keys())
     if st.session_state.selected_location != location_name:
         # Clear the previous selection and re-initialize the graph
