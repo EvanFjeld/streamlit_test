f7c0938215655007fb2f7cdbc4fc88d6758562e5
EvanFjeld <evanmfjeld@gmail.com>
2023-07-28 18:42:15 -0700

    Update homepage_2.py

--- homepage_2.py
+++ homepage_2.py
@@ -122,7 +122,7 @@ pages = {
     "About": about_page
 }
 
-single_location_to_funcs = single_location_functions()
+single_location_to_funcs = pag_names_functions()
 
 # Check if the app is just starting (no state exists)
 if "initialized" not in st.session_state:
