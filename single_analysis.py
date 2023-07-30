61a85b491e88ea3a9076cb38aede82e7640365c9
EvanFjeld <evanmfjeld@gmail.com>
2023-07-29 17:17:19 -0700

    Update single_analysis.py

--- single_analysis.py
+++ single_analysis.py
@@ -138,7 +138,7 @@ st.markdown(
 
 #st.sidebar.button("About")
 location_name = st.selectbox("Choose a location", options.keys())
-location_filename = locations_df.loc[(options_df["Location"] == location_name), "filename"].values[0]
+location_filename = options_df.loc[(options_df["Location"] == location_name), "filename"].values[0]
 #page_names_to_funcs[location_name][1](page_names_to_funcs[location_name][0], location_name)
 single_location_analysis(location_filename, location_name)
 
