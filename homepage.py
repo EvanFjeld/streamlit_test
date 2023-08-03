9946650c6fe6c7801f4038ca4de21ba3518f2f6c
EvanFjeld <evanmfjeld@gmail.com>
2023-08-02 21:38:16 -0700

    Update homepage.py

--- homepage.py
+++ homepage.py
@@ -22,8 +22,15 @@ show_pages(
 
 st.write("# Welcome to the Boreal Forest Carbon Calculator")
 
+# Display the image
+AWS_BUCKET_URL = "https://carbon-forecaster-capstone-s3.s3.us-west-2.amazonaws.com"
+image_name = "/streamlit_data/about/boreal_forest.jpeg"
+st.image(AWS_BUCKET_URL + image_name, caption='Boreal Forest', use_column_width=True)
+
+# Text
 st.markdown(
     """
-    Single location or compare mulitple locations. 
+    This app is meant to facilitate the exploration of Gross Primary Productivity (GPP) of Canada's Boreal Foreast. Using the MODIS dataset and a techniqule called Temporally-Fused
+    Transformers (TFT) time-series modeling, we are able to look at GPP values in the Boreal Forest over the past 20 years to predict the next 30. 
 """
 )
