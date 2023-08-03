8b3e198acef8dd6ce83d10b62cd0c1f7eeff705c
EvanFjeld <evanmfjeld@gmail.com>
2023-08-02 20:45:57 -0700

    Update single_analysis.py

--- testing/single_analysis.py
+++ testing/single_analysis.py
@@ -93,12 +93,11 @@ def single_location_analysis(file, location):
             'Gpp': 'mean',
             'isforecasted': lambda x: any(x)  # Check if any value in 'isforecasted' is True
         }).reset_index()
-        st.write("Preiod:", time_frame)
     else:
         filtered_df = df
     
     # Filter the DataFrame based on the selected date range
-    filtered_df = df[(df.date >= start_date) & (df.date <= end_date)]
+    filtered_df = filtered_df[(df.date >= start_date) & (df.date <= end_date)]
 
     # Create the plot
     fig, ax = plt.subplots(figsize=(8, 6))
