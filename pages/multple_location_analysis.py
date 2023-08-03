36e059ab4a61afb7d866b0cbd9efa35c36de4ea1
EvanFjeld <evanmfjeld@gmail.com>
2023-08-02 20:56:57 -0700

    Update multple_location_analysis.py

--- pages/multple_location_analysis.py
+++ pages/multple_location_analysis.py
@@ -90,7 +90,8 @@ def multiple_location_analysis(file1, file2, location1, location2):
     # group dataset for time period
     if time_frame == "Yearly":
         filtered_df = df.groupby(df['date'].dt.year).agg({
-            'Gpp': 'mean',
+            'Gpp_loc1': 'mean',
+            'Gpp_loc2': 'mean',
             'isforecasted': lambda x: any(x)  # Check if any value in 'isforecasted' is True
         }).reset_index()
         # Filter the DataFrame based on the selected date range
